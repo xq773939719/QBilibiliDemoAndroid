bd7ce1234d718a05a45d83b35ac5621a4e4074e1
xq773939719 <773939719@qq.com>
2021-01-21 15:54:41 +0800

    update

--- app/src/main/java/com/xq/bilibilidemo/activity/DemoActivityOfIJKPlayer.java
+++ app/src/main/java/com/xq/bilibilidemo/activity/DemoActivityOfIJKPlayer.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.xq.bilibilidemo.R;
+import com.xq.bilibilidemo.view.CustomPlayerWithIJKPlayer;
 
-import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+import java.io.IOException;
 
-public class DemoActivityOfIJKPlayer extends AppCompatActivity {
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+
+public class DemoActivityOfIJKPlayer extends AppCompatActivity implements CustomPlayerWithIJKPlayer.VideoListener {
+
+    private CustomPlayerWithIJKPlayer videoPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,53 @@ public class DemoActivityOfIJKPlayer extends AppCompatActivity {
         setContentView(R.layout.activity_demo_of_i_j_k_player);
 
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//黑色
+
+        videoPlayer = findViewById(R.id.custom_ijkplayer);
+
+        videoPlayer.setVideoListener(this);
+
+        videoPlayer.setPath("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+        try {
+            videoPlayer.load();
+        } catch (IOException e) {
+            Toast.makeText(this, "播放失败", Toast.LENGTH_SHORT);
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onBufferingUpdate(IMediaPlayer iMediaPlayer, int i) {
+
+    }
+
+    @Override
+    public void onCompletion(IMediaPlayer iMediaPlayer) {
+
+    }
+
+    @Override
+    public boolean onError(IMediaPlayer iMediaPlayer, int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean onInfo(IMediaPlayer iMediaPlayer, int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(IMediaPlayer iMediaPlayer) {
+        videoPlayer.start();
+    }
+
+    @Override
+    public void onSeekComplete(IMediaPlayer iMediaPlayer) {
+
+    }
+
+    @Override
+    public void onVideoSizeChanged(IMediaPlayer iMediaPlayer, int i, int i1, int i2, int i3) {
 
     }
 }
